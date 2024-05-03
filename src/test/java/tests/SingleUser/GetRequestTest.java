0c1af14410bb747acdc262c87150b6ca04f25b6b
radioactive1337 <vysotskiidanil@gmail.com>
2024-05-03 16:22:45 +0600

    get user add test

--- src/test/java/tests/SingleUser/GetRequestTest.java
+++ src/test/java/tests/SingleUser/GetRequestTest.java
@@ -13,11 +13,12 @@ import tests.BaseTest;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.Specs.requestSpec;
 import static specs.Specs.responseSpec;
 
 @Epic("Users")
-@Feature("Get user")
+@Feature("Get user by username smoke test")
 public class GetRequestTest extends BaseTest {
     User testUser = new User(666, "Quattro", "Sam", "Something", "gmail@gmail.com", "qwerty123", "88805050707", 3);
 
@@ -40,7 +41,7 @@ public class GetRequestTest extends BaseTest {
                 .spec(responseSpec(200))
                 .assertThat()
                 .body(matchesJsonSchemaInClasspath("singleUserSchema.json"))
-                .body("id", equalTo(666))
+                .body("id", equalTo(testUser.id()))
                 .body("username", equalTo(testUser.username()))
                 .body("firstName", equalTo(testUser.firstName()))
                 .body("lastName", equalTo(testUser.lastName()))
@@ -50,11 +51,23 @@ public class GetRequestTest extends BaseTest {
                 .body("userStatus", equalTo(testUser.userStatus()));
     }
 
-    //  test with extraction to class
     @Test
     @Severity(SeverityLevel.BLOCKER)
     @DisplayName("Get user by username smoke test")
     public void getUserWithPojo() {
-
+        User userResponse = RestAssured.given()
+                .spec(requestSpec())
+                .get(String.format("user/%s", testUser.username()))
+                .then()
+                .spec(responseSpec(200))
+                .extract().as(User.class);
+        assertEquals(userResponse.id(), testUser.id());
+        assertEquals(userResponse.username(), testUser.username());
+        assertEquals(userResponse.firstName(), testUser.firstName());
+        assertEquals(userResponse.lastName(), testUser.lastName());
+        assertEquals(userResponse.email(), testUser.email());
+        assertEquals(userResponse.password(), testUser.password());
+        assertEquals(userResponse.phone(), testUser.phone());
+        assertEquals(userResponse.userStatus(), testUser.userStatus());
     }
 }
